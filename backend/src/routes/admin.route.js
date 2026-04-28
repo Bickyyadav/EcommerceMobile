3fbb46fbe49d774ea17cbcdcd45bee92a444d3ec
Vicky Yadav <ybicky86@gmail.com>
2026-04-28 15:17:22 +0530

    admin all routes completed

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { adminOnly, protectRoute } from "../middleware/auth.middleware";
-import { createProducts, deleteProduct, getAllProducts, updateProduct } from "../controllers/admin.controller";
+import { createProducts, deleteProduct, getAllOrders, getAllProducts, updateOrderStatus, updateProduct } from "../controllers/admin.controller";
 import { upload } from "../middleware/multer.middleware";
 
 
@@ -8,11 +8,23 @@ const router = Router();
 
 router.use(protectRoute, adminOnly)
 
+//for create products
 //use this images name in the frontend 
 router.post("/products", upload.array("images", 3), createProducts)
 router.get("/products", getAllProducts)
 router.put("/products/:id",upload.array("images", 3), updateProduct)
 router.delete("/products/:id", deleteProduct)
+// get all orders
+router.get("/orders", getAllOrders)
+// update order status from pending to shift and shift to delivery
+router.patch("/orders/:orderId/status", updateOrderStatus)
+// get all customer
+router.get("/customers", getAllCustomers)
+router.get("/stats", getDashboardStats)
 
+
+// different between order and patch
+//PUT: in put the admin will update the entire product details like name , description , image and other all thigns 
+//Patch: in pathc only some part like shirt to delivery and delivery to cancel 
 
 export default router;
